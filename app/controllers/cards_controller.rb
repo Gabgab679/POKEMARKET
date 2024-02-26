6134d888ad07a32b5b6ab9ff234328aa4460abe8
antonin danto <antonindanto@gmail.com>
2024-02-26 18:08:26 +0100

    premier commit du update Cardscontroller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,13 +1,25 @@
 class CardsController < ApplicationController
+  before_action :user_signed_in?
   def index
+    @cards = Card.all.select { |card| card.user_id = current_user.id }
   end
 
   def show
+    @card = Card.find(params[:card_id])
   end
 
   def new
+    @card = Card.new
   end
 
   def create
+    @card = Card.new(card_params)
+    @card.user_id = current_user.id
+  end
+
+  private
+
+  def card_params
+    params.require(:flat).permit(:name, :description, :image, :state, :price)
   end
 end
