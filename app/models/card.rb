49f7dec4f5e29c9275bd3ad12e6d298a4842a921
gabrielle simha <gabrielle.simha@gmail.com>
2024-02-27 17:05:42 +0100

    add validates

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,10 +4,10 @@ class Card < ApplicationRecord
   belongs_to :user #Seller
   has_one :sale
 
-  # validates :name, presence: true
-  # validates :description, presence: true
-  # validates :state, presence: true, inclusion: { in: STATES }
-  # validates :price, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :state, presence: true, inclusion: { in: STATES }
+  validates :price, presence: true
 
   has_many_attached :photos
 end
