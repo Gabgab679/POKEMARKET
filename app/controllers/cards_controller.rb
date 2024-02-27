6414e91b7f059afeb81bfe040b9fba0ea9c4cc92
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2024-02-27 11:49:08 +0100

    add login/logout flow with devise

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,6 @@
 class CardsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index # Saute l'étape d'auth sur l'action index (puisque c'est notre root)
+
   def index
   end
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  before_action :user_signed_in?, only: [:home]
   def home
     # on veut renvoyer vers la page url/cards
     # Commentaire d'Alex > Si on renvoie vers url/cards, on utilise redirect_to ?
+  end
+
+  def dashboard
+    # specs?
   end
 end
